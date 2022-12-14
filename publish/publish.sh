b31f51f403140bb64e345796101547178df84ae3
Damian Bruera <brueradamian@gmail.com>
2022-12-14 12:42:30 -0300

    Fixing mountebank server host

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until ping -c 1 mountebank; do
+until ping -c 1 mountebank-svc; do
   echo "Waiting for mountebank server to be ready...."
   sleep 2
 done
@@ -8,5 +8,5 @@ done
 
 for filename in definition/*.json; do
   printf "Loading %s.json\n" $(basename "$filename" .json)
-  curl -X POST -H "Content-Type: application/json" -d @definition/$(basename "$filename" .json).json http://mountebank:2525/imposters
+  curl -X POST -H "Content-Type: application/json" -d @definition/$(basename "$filename" .json).json http://mountebank-svc:2525/imposters
 done
